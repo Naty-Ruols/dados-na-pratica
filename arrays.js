af7afa10ba964bde0d7d925f46e4e3e0492d2f7c
Natalia Cerqueira <153464979+Naty-Ruols@users.noreply.github.com>
2024-01-27 19:36:13 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,12 @@
+var valores = [3,5,2,5,1];
+
+console.log(valores[0]);
+
+for (var pos = 0 ; pos < valores.length; pos++){
+    console.log ("Posição:" + pos + "Valor:" +[pos])
+}
+
+console.log("array invertido:" + valores.reverse());
+
+var arr1 = [1,2,3,4,5]
+console.log(arr1.join(' é menor que '));
